719ba691830ece0362b501e98b19205c9aa4fbf4
Goldberg001 <pasaribu766hi@gmail.com>
2024-07-19 16:50:42 +0700

    Model & Controller

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,68 @@
+<div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="{{ asset('dist/img/avatar5.png') }}" class="img-circle elevation-2" alt="User Image">
+      </div>
+      <div class="info">
+        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+      </div>
+    </div>
+  
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+          <a href="{{ route('dashboard') }}" class="nav-link @if (request()->routeIs('dashboard')) active @endif">
+            <i class="nav-icon fas fa-desktop"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <li class="nav-item @if (request()->routeIs('mobil.*') || request()->routeIs('penyewa.*') || request()->routeIs('supir.*'))) menu-open @endif">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-database"></i>
+            <p>Mobil & Supir
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="{{ route('mobil.index') }}" class="nav-link @if (request()->routeIs('mobil.*')) active @endif">
+                <i class="nav-icon fas fa-car"></i>
+                <p>Mobil</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ route('supir.index') }}" class="nav-link @if (request()->routeIs('supir.*')) active @endif">
+                <i class="nav-icon fas fa-user-tie"></i>
+                <p>Supir Mobil</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a href="{{ route('transaksi.index') }}" class="nav-link @if (request()->routeIs('transaksi.*')) active @endif">
+            <i class="nav-icon fas fa-money-check"></i>
+            <p>Kebun</p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="{{ route('kebun.index') }}" class="nav-link @if (request()->routeIs('mobil.*')) active @endif">
+                <i class="nav-icon fas fa-car"></i>
+                <p>Perkebunan Utama</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ route('afd.index') }}" class="nav-link @if (request()->routeIs('supir.*')) active @endif">
+                <i class="nav-icon fas fa-user-tie"></i>
+                <p>Afdeling Kebun</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+  
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +15,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Route untuk mobil
+    Route::get('/mobil', [MobilController::class, 'index'])->name('mobil.index');
+    Route::get('/mobil/create', [MobilController::class, 'create'])->name('mobil.create');
+    Route::post('/mobil', [MobilController::class, 'store'])->name('mobil.store');
+    Route::get('/mobil/{mobil}', [MobilController::class, 'show'])->name('mobil.show');
+    Route::get('/mobil/{mobil}/edit', [MobilController::class, 'edit'])->name('mobil.edit');
+    Route::put('/mobil/{mobil}', [MobilController::class, 'update'])->name('mobil.update');
+    Route::delete('/mobil/{mobil}', [MobilController::class, 'destroy'])->name('mobil.destroy');
 });
 
 require __DIR__.'/auth.php';
